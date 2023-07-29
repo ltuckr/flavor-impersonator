1833c1b2f90178e8151e7c2ecef92502c9671b5f
DonnaThompson7 <130340823+DonnaThompson7@users.noreply.github.com>
2023-07-29 17:53:21 -0400

    added homepage, login code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
-const commentsRoutes = require('./commentsRoutes');
-const recipeRoutes = require('./recipeRoutes');
+// const commentRoutes = require('./commentRoutes');
+// const recipeRoutes = require('./recipeRoutes');
 
 router.use('/users', userRoutes);
-router.use('/comments', commentsRoutes);
-router.use('/recipe', recipeRoutes);
+// router.use('/comments', commentRoutes);
+// router.use('/recipe', recipeRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,7 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes')
-const userRoutes = require('./userRoutes');
-
 const apiRoutes = require('./api');
 
-router.use('/users', userRoutes);
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes); 
 
