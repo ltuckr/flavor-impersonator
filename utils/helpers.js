2593d88d3ed1085d081f22110d1a7e6d4101362f
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-07-31 19:33:22 -0400

    remove redundant validator helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-// validate.js to check for valid URLs and email addresses
-const validator = require('validator');
 
-function isValidEmail(email) {
-  return validator.isEmail(email);
-}
-
-function isValidURL(url) {
-  return validator.isURL(url);
-}
-
-
-
-module.exports = {
-  isValidEmail,
-  isValidURL,
-
-};
-=======
 module.exports = {
     format_date: (date) => {
       // Format date as MM/DD/YYYY
@@ -25,4 +6,4 @@ module.exports = {
     },
   };
   
->>>>>>> a936622d05d770e6e0303e0b694e86e60fdce1ce
+
