7af605986dff91815ed88b393193e0a259f47af6
DonnaThompson7 <130340823+DonnaThompson7@users.noreply.github.com>
2023-08-01 01:38:44 -0400

    Added login, logout, signup, profile and part of browse recipes functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 // const commentRoutes = require('./commentRoutes');
-// const recipeRoutes = require('./recipeRoutes');
+const recipeRoutes = require('./recipeRoutes');
 
 router.use('/users', userRoutes);
 // router.use('/comments', commentRoutes);
-// router.use('/recipe', recipeRoutes);
+router.use('/recipes', recipeRoutes);
 
 module.exports = router;
