4b53ba6772d352517a1e63b33ea607457c1b1f03
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-07-31 20:38:55 -0400

    removed helmet for now. errors

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,16 +2,11 @@
 function format_date(date) {
     return date.toLocaleDateString();
   }
-  
-  // Function to generate the CSP header value
-  function generateCSP() {
-    const cspValue = "default-src 'self'; script-src 'self' https://trusted-cdn.com; style-src 'self' https://trusted-cdn.com; img-src 'self' data: https://trusted-cdn.com; font-src 'self' https://trusted-cdn.com; media-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self';";
-    return cspValue;
-  }
+
   
   // Export the functions
   module.exports = {
     format_date,
-    generateCSP,
+   
   };
   
