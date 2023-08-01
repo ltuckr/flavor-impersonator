e4c5e33c3e1251d1e87a247a01dac08780c690f4
Lauren Tucker <l.tucker.rdh@gmail.com>
2023-07-31 20:52:16 -0400

    removed helmet b/c errors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-//helmet was here
+//helmet was here.
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
