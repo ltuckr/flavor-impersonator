226b252a766750793af773c6d172e5ec251feedb
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-07-31 20:26:01 -0400

    homepage css and styling -Ian

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(helmet());
+// app.use(helmet());
 
 app.use(routes);
 
